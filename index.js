c7e3fb0a7b2abd7c1816bf5fe95b2aa29b5b1d0b
Abhishek Kumar <prajapatiabhishek02@gmail.com>
2023-09-15 20:01:14 +0530

    Update index.js
    2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const axios = require('axios');
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 const cors = require("cors");
 app.use(express.json());
 app.use(cors());
